04a3769861788e2977efcc7722d06732eabfd4b9
3103198712 <jeffersonv.nascimento@gmail.com>
2025-07-14 15:26:43 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 const verifyToken = process.env.VERIFY_TOKEN;
 
 app.get('/', (req, res) => {
-  res.send('Seu token de verificação é: ${verifyToken});
+  res.send('Seu token de verificação é: ${verifyToken}');
 });
 
 app.listen(port, () => {
