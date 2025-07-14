3e80ee3cdffb9577f9e357248d388fabcc27dc50
3103198712 <jeffersonv.nascimento@gmail.com>
2025-07-14 14:06:48 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+const verifyToken = process.env.VERIFY_TOKEN;
+
+app.get('/', (req, res) => {
+  res.send(Seu token de verificação é: ${verifyToken});
+});
+
+app.listen(port, () => {
+  console.log(Servidor rodando na porta ${port}, token=${verifyToken});
+});
