48f922352e0520f77c1a90e062b41e8836cac1af
3103198712 <jeffersonv.nascimento@gmail.com>
2025-07-14 15:30:52 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(Servidor rodando na porta ${port}, token=${verifyToken}');
+  console.log('Servidor rodando na porta ${port}, token=${verifyToken}');
 });
